3cd77ecb5653fcc879cf0f5b0d245a740ebc7f84
Kübra Terzi <kubraterzi0303@gmail.com>
2022-05-19 20:43:49 +0300

    coded Sign page

--- Client/src/components/Sign.jsx
+++ Client/src/components/Sign.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./bars/Navbar";
 
 const Sign = () => {
   const [currentForm, setCurrentForm] = React.useState("loginForm");
@@ -15,120 +16,125 @@ const Sign = () => {
     setCurrentForm("loginForm");
   };
   return (
-    <section className="user">
-      <div className="user_options-container">
-        <div className="user_options-text">
-          <div className="user_options-unregistered">
-            <h2 className="user_unregistered-title">Don't have an account?</h2>
-            <p className="user_unregistered-text">
-              Banjo tote bag bicycle rights, High Life sartorial cray craft beer
-              whatever street art fap.
-            </p>
-            <button
-              className="user_unregistered-signup"
-              id="signup-button"
-              onClick={handleSignUp}
-            >
-              Sign up
-            </button>
+    <>
+      <Navbar />
+      <section className="user">
+        <div className="user_options-container">
+          <div className="user_options-text">
+            <div className="user_options-unregistered">
+              <h2 className="user_unregistered-title">
+                Don't have an account?
+              </h2>
+              <p className="user_unregistered-text">
+                Banjo tote bag bicycle rights, High Life sartorial cray craft
+                beer whatever street art fap.
+              </p>
+              <button
+                className="user_unregistered-signup"
+                id="signup-button"
+                onClick={handleSignUp}
+              >
+                Sign up
+              </button>
+            </div>
+
+            <div className="user_options-registered">
+              <h2 className="user_registered-title">Have an account?</h2>
+              <p className="user_registered-text">
+                Banjo tote bag bicycle rights, High Life sartorial cray craft
+                beer whatever street art fap.
+              </p>
+              <button
+                className="user_registered-login"
+                id="login-button"
+                onClick={handleSignIn}
+              >
+                Login
+              </button>
+            </div>
           </div>
 
-          <div className="user_options-registered">
-            <h2 className="user_registered-title">Have an account?</h2>
-            <p className="user_registered-text">
-              Banjo tote bag bicycle rights, High Life sartorial cray craft beer
-              whatever street art fap.
-            </p>
-            <button
-              className="user_registered-login"
-              id="login-button"
-              onClick={handleSignIn}
-            >
-              Login
-            </button>
+          <div className="user_options-forms" id="user_options-forms">
+            {currentForm === "loginForm" && (
+              <div className="user_forms-login" id="user_forms-login">
+                <h2 className="forms_title">Login</h2>
+                <form className="forms_form">
+                  <fieldset className="forms_fieldset">
+                    <div className="forms_field">
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className="forms_field-input"
+                        required
+                        autoFocus
+                      />
+                    </div>
+                    <div className="forms_field">
+                      <input
+                        type="password"
+                        placeholder="Password"
+                        className="forms_field-input"
+                        required
+                      />
+                    </div>
+                  </fieldset>
+                  <div className="forms_buttons">
+                    <button type="button" className="forms_buttons-forgot">
+                      Forgot password?
+                    </button>
+                    <input
+                      type="submit"
+                      value="Log In"
+                      className="forms_buttons-action"
+                    />
+                  </div>
+                </form>
+              </div>
+            )}
+            {currentForm === "signupForm" && (
+              <div className="user_forms-signup">
+                <h2 className="forms_title">Sign Up</h2>
+                <form className="forms_form">
+                  <fieldset className="forms_fieldset">
+                    <div className="forms_field">
+                      <input
+                        type="text"
+                        placeholder="Full Name"
+                        className="forms_field-input"
+                        required
+                      />
+                    </div>
+                    <div className="forms_field">
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className="forms_field-input"
+                        required
+                      />
+                    </div>
+                    <div className="forms_field">
+                      <input
+                        type="password"
+                        placeholder="Password"
+                        className="forms_field-input"
+                        required
+                      />
+                    </div>
+                  </fieldset>
+                  <div className="forms_buttons">
+                    <input
+                      type="submit"
+                      value="Sign up"
+                      className="forms_buttons-action"
+                    />
+                  </div>
+                </form>
+              </div>
+            )}
           </div>
         </div>
-
-        <div className="user_options-forms" id="user_options-forms">
-          {currentForm === "loginForm" && (
-            <div className="user_forms-login" id="user_forms-login">
-              <h2 className="forms_title">Login</h2>
-              <form className="forms_form">
-                <fieldset className="forms_fieldset">
-                  <div className="forms_field">
-                    <input
-                      type="email"
-                      placeholder="Email"
-                      className="forms_field-input"
-                      required
-                      autoFocus
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <input
-                      type="password"
-                      placeholder="Password"
-                      className="forms_field-input"
-                      required
-                    />
-                  </div>
-                </fieldset>
-                <div className="forms_buttons">
-                  <button type="button" className="forms_buttons-forgot">
-                    Forgot password?
-                  </button>
-                  <input
-                    type="submit"
-                    value="Log In"
-                    className="forms_buttons-action"
-                  />
-                </div>
-              </form>
-            </div>
-          )}
-          {currentForm === "signupForm" && (
-            <div className="user_forms-signup">
-              <h2 className="forms_title">Sign Up</h2>
-              <form className="forms_form">
-                <fieldset className="forms_fieldset">
-                  <div className="forms_field">
-                    <input
-                      type="text"
-                      placeholder="Full Name"
-                      className="forms_field-input"
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <input
-                      type="email"
-                      placeholder="Email"
-                      className="forms_field-input"
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <input
-                      type="password"
-                      placeholder="Password"
-                      className="forms_field-input"
-                      required
-                    />
-                  </div>
-                </fieldset>
-                <div className="forms_buttons">
-                  <input
-                    type="submit"
-                    value="Sign up"
-                    className="forms_buttons-action"
-                  />
-                </div>
-              </form>
-            </div>
-          )}
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
