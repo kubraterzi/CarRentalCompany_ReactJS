8d12ce334488568fc75af110400b94d12c68719a
Kübra Terzi <kubraterzi0303@gmail.com>
2022-05-21 15:23:09 +0300

    filtered cars by search bar

--- Client/src/components/cars/CarList.jsx
+++ Client/src/components/cars/CarList.jsx
@@ -1,21 +1,92 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useSelector } from "react-redux";
 
 import UserLayout from "../layouts/UserLayout";
 import CarItemCard from "./CarItemCard";
+import {
+  fetchAllCars,
+  fetchFilteredCars,
+} from "./../../redux/slices/cars/carsSlice";
 import Filter from "../utilities/Filter";
+import { AppBar, Toolbar } from "@mui/material";
 
 const CarList = () => {
+  const keys = ["brandName", "colorName", "brandModel", "dailyPrice"];
+
+  const [filteredCars, setFilteredCars] = React.useState([]);
+  const [filteredSearch, setFilteredSearch] = React.useState("");
+  const selectedBrand = useSelector((state) => state.brands.currentBrand);
+  const selectedColor = useSelector((state) => state.colors.currentColor);
+  const selectedStatus = useSelector((state) => state.statuses.currentStatus);
+
+  const filterBySearchBar = (data) => {
+    return data.filter(
+      (item) =>
+        keys.some((key) =>
+          item[key].toString().toLowerCase().includes(filteredSearch)
+        ) // satırlarca or sorgusu yazmak yerine filtrelerken olabilecekleri listeleyen method some metodudur.
+    );
+  };
+
+  const filteredCarsBySearch = filterBySearchBar(filteredCars);
+
+  useEffect(() => {
+    if (selectedBrand === undefined && selectedColor === undefined) {
+      const fetchData = async () => {
+        const allCars = await fetchAllCars();
+        setFilteredCars(allCars);
+      };
+      fetchData();
+    } else {
+      const colorId = selectedColor?.colorID;
+      const brandId = selectedBrand?.brandID;
+      const filters =
+        brandId && colorId
+          ? {
+              brandId, // : iki tarafı da aynı olduğu için tek sefer yazmak yeterli/  brandId : brandId ile aynı
+              colorId,
+            }
+          : brandId
+          ? { brandId }
+          : { colorId };
+
+      const fetchData = async () => {
+        const filteredCarsByBrand = await fetchFilteredCars(filters);
+        setFilteredCars(filteredCarsByBrand);
+      };
+      fetchData();
+    }
+  }, [selectedBrand, selectedColor]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const filteredCarsByStatus = await fetchFilteredCars({
+        status: selectedStatus,
+      });
+      setFilteredCars(filteredCarsByStatus);
+    };
+    fetchData();
+  }, [selectedStatus]);
+
   return (
     <div>
       <UserLayout>
-        <Filter />
+        <AppBar
+          position="static"
+          sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+        >
+          <Toolbar>
+            <Filter
+              filteredSearch={filteredSearch}
+              setFilteredSearch={setFilteredSearch}
+            />{" "}
+          </Toolbar>
+        </AppBar>
         <div className="card-items">
-          <CarItemCard />
-          <CarItemCard />
-          <CarItemCard />
-          <CarItemCard />
-          <CarItemCard />
-          <CarItemCard />
+          {filteredCarsBySearch.map((car) => (
+            <CarItemCard key={car.carID} car={car} />
+          ))}
         </div>
       </UserLayout>
     </div>
--- Client/src/components/utilities/Filter.jsx
+++ Client/src/components/utilities/Filter.jsx
@@ -1,67 +1,73 @@
 import React from "react";
 
-import { alpha, AppBar, InputBase, Toolbar } from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
+
 import Switcher from "./Switcher";
-import { styled } from "@mui/material/styles";
 
-const Filter = () => {
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.black, 0.55),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.black, 0.65),
-    },
+const Search = styled("div")(({ theme }) => ({
+  color: "white",
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.55),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.65),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
     width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(2),
-      width: "auto",
+    [theme.breakpoints.up("md")]: {
+      width: "62ch",
     },
-  }));
+  },
+}));
 
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "62ch",
-      },
-    },
-  }));
+const Filter = (props) => {
+  const handleChange = (e) => {
+    props.setFilteredSearch(e.target.value);
+    // console.log(e.target.value);
+  };
 
   return (
-    <AppBar
-      position="static"
-      sx={{ backgroundColor: "transparent", boxShadow: "none" }}
-    >
-      <Toolbar>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ "aria-label": "search" }}
-          />
-        </Search>
-        <Switcher />
-      </Toolbar>
-    </AppBar>
+    <>
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+          value={props.filteredSearch}
+          onChange={handleChange}
+        />
+      </Search>
+      <Switcher />
+    </>
   );
 };
 
