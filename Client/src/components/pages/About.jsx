d79d8ebb4d2ddc87cefb7dffe41f9f1e8155e2c8
Kübra Terzi <kubraterzi0303@gmail.com>
2022-05-26 17:28:09 +0300

    coded about and contact pages

--- Client/src/components/pages/About.jsx
+++ Client/src/components/pages/About.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Navbar from "../bars/Navbar";
+import Copyright from "../utilities/Copyright";
+import SocialMediaArea from "../utilities/SocialMediaArea";
+import ceo from "../../assets/images/about-page-images/ceo.jpeg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Navbar />
+      <hr />
+      <main className="about">
+        <div className="container">
+          <section className="grid">
+            <div className="info">
+              <div className="about">
+                <h1 className="section-heading">About Me</h1>
+              </div>
+              <div className="img-ml">
+                <img src={ceo} />
+              </div>
+              <div>
+                <div className="intro">
+                  <h2>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Aliquid, repellat!
+                  </h2>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Reprehenderit assumenda eveniet magnam earum? Voluptates
+                    voluptas beatae placeat reiciendis tenetur odio soluta,
+                    quasi ipsum quod vel temporibus quibusdam sunt rem unde.
+                  </p>
+                  <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus atque perspiciatis ratione voluptatem perferendis odio
+                    odit dicta velit neque officiis! Quia dolorem numquam
+                    officiis eius consequatur cumque quam facere aspernatur?
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quae, aut?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+      <SocialMediaArea />;
+      <Copyright />
+    </>
+  );
 };
 
 export default About;
--- Client/src/components/pages/Contact.jsx
+++ Client/src/components/pages/Contact.jsx
@@ -1,7 +1,102 @@
 import React from "react";
 
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import SendIcon from "@mui/icons-material/Send";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from "@mui/icons-material/Email";
+import Navbar from "./../bars/Navbar";
+import SocialMediaArea from "../utilities/SocialMediaArea";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Navbar />
+      <section id="contact">
+        <h1 className="section-header"> Contact </h1>
+
+        <div className="contact-wrapper">
+          <form id="contact-form" className="form-horizontal" role="form">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  placeholder="NAME"
+                  name="name"
+                  value=""
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="EMAIL"
+                  name="email"
+                  value=""
+                  required
+                />
+              </div>
+            </div>
+
+            <textarea
+              className="form-control"
+              rows="10"
+              placeholder="MESSAGE"
+              name="message"
+              required
+            ></textarea>
+
+            <button
+              className="btn btn-primary send-button"
+              id="submit"
+              type="submit"
+              value="SEND"
+            >
+              <div className="alt-send-button">
+                <SendIcon sx={{ fontSize: "x-large", marginTop: "4px" }} />
+                <span className="send-text">SEND</span>
+              </div>
+            </button>
+          </form>
+
+          <div className="direct-contact-container">
+            <ul className="contact-list">
+              <li className="list-item">
+                <LocationOnIcon sx={{ fontSize: "x-large" }} />
+                <span className="contact-text place">İstanbul, Turkey</span>
+              </li>
+
+              <li className="list-item">
+                <LocalPhoneIcon sx={{ fontSize: "x-large" }} />
+                <span className="contact-text phone">
+                  <a href="tel:1-212-555-5555" title="Give me a call">
+                    (212) 555-2368
+                  </a>
+                </span>
+              </li>
+
+              <li className="list-item">
+                <EmailIcon sx={{ fontSize: "x-large" }} />
+                <span className="contact-text gmail">
+                  <a href="mailto:#" title="Send me an email">
+                    ktrental@gmail.com
+                  </a>
+                </span>
+              </li>
+            </ul>
+            <SocialMediaArea />
+            <div className="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
